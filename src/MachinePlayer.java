e1eb468238d6302a89188c41246fd86d44d7e6ff
eitan chervits <eitan.chervits33@gmail.com>
2020-04-25 14:55:40 +0300

    new version

--- src/MachinePlayer.java
+++ src/MachinePlayer.java
@@ -1,18 +1,19 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 
-public class MachinePlayer extends Player{
+public class MachinePlayer extends Player {
     private Color color;
-    private Checker [] myCheckers = new Checker[12];
+    private Checker[] myCheckers = new Checker[12];
     private Board board;
 
-    public MachinePlayer(Color color,Board boardOG) {
+    public MachinePlayer(Color color, Board boardOG) {
         super(color);
         this.color = color;
         this.board = new Board(boardOG);
         this.myCheckers = boardOG.getPlayerByColor(color).getMyCheckers();
 
     }
+
     int eval(Board game) {
         if (game.piecesContiguous(board.getPlayerByColor(color))) {
             return Integer.MAX_VALUE;//returns the value of +infinity
@@ -22,12 +23,12 @@ public class MachinePlayer extends Player{
         }
         ArrayList<Position> human = new ArrayList<Position>();
         ArrayList<Position> computer = new ArrayList<Position>();
-        for (Checker[] checkerArray: board.getCheckers()) {
-            for (Checker c: checkerArray) {
+        for (Checker[] checkerArray : board.getCheckers()) {
+            for (Checker c : checkerArray) {
                 if (c.getColor() == color) {
                     Position position = new Position(c.getPosition());
                     computer.add(position);
-                } else if (c.getColor()!=color) {
+                } else if (c.getColor() != color) {
                     Position position = new Position(c.getPosition());
                     human.add(position);
                 }
@@ -37,15 +38,16 @@ public class MachinePlayer extends Player{
         int hum = distance(human);
         return hum - comp;
     }
+
     int distance(ArrayList<Position> pieces) {
         double twopiecedist;
         int sum = 0;
         double x1;
         double y1;
-        for (Position positionI:pieces) {
-            for (Position positionJ:pieces) {
-                x1 = Math.pow(positionI.getX()-positionJ.getX(), 2);
-                y1 = Math.pow(positionI.getY()-positionJ.getY(), 2);
+        for (Position positionI : pieces) {
+            for (Position positionJ : pieces) {
+                x1 = Math.pow(positionI.getX() - positionJ.getX(), 2);
+                y1 = Math.pow(positionI.getY() - positionJ.getY(), 2);
                 twopiecedist =
                         Math.sqrt(x1 + y1);
                 sum += Math.round(twopiecedist);
@@ -53,18 +55,19 @@ public class MachinePlayer extends Player{
         }
         return sum;
     }
+
     public Position minmax(Color Color, int depth, int alpha, int beta) {
         Position best = null;
         int bestmove = Integer.MIN_VALUE;
         int current = Integer.MIN_VALUE;
-        ArrayList<Position> list=null;
-        Game gameCopy = new Game(board,color);
-        for (Checker c: board.getPlayerByColor(color).getMyCheckers()) {
+        ArrayList<Position> list = null;
+        Game gameCopy = new Game(board, color);
+        for (Checker c : board.getPlayerByColor(color).getMyCheckers()) {
             int index = 0;
             if (c != null)
                 list = board.canGo(c);
             while (!list.isEmpty()) {
-                gameCopy.makeAMove(c,list.get(index));
+                gameCopy.makeAMove(c, list.get(index));
                 current =
                         alphabeta(color, gameCopy, alpha, beta, depth - 1);
                 if (current > bestmove) {
@@ -77,43 +80,48 @@ public class MachinePlayer extends Player{
         }
         return best;
     }
+
     int alphabeta(Color color, Game game, int alpha, int beta, int depth) {
-        if (game.GameOver()!=null || depth <= 0) {
+        if (game.GameOver() != null || depth <= 0) {
             return eval(board);
         }
         int score;
-        Board copy = new Board(game.getcontents(), game.getplayer());
-        Iterator<Move> iter = copy.legalMoves();
-        ArrayList<Board> state = new ArrayList<Board>();
-        while (iter.hasNext()) {
-            copy.makeMove(iter.next());
-            state.add(copy);
-            copy.retract();
-        }
-        if (game.getplayer() == side()) {
-            for (Board b : state) {
-                score = alphabeta(side().opponent(), b, alpha, beta, depth - 1);
-                if (score > alpha) {
-                    alpha = score;
-                }
-                if (alpha >= beta) {
-                    return alpha;
-                }
+        Game gameCopy = new Game(board, color);
+        ArrayList<Game> state = new ArrayList<Game>();
+        ArrayList<Position> list = new ArrayList<Position>();
+        for (Checker c : board.getPlayerByColor(color).getMyCheckers()) {
+            int index = 0;
+            if (c != null)
+                list = board.canGo(c);
+            while (!list.isEmpty()) {
+                gameCopy.makeAMove(c, list.get(index));
+                state.add(gameCopy);
             }
-            return alpha;
-        } else {
-            for (Board b : state) {
-                score =
-                        alphabeta(side().opponent(), b, alpha, beta, depth - 1);
-                if (score < beta) {
-                    beta = score;
+        }
+            if (board.getPlayerByColor(color) == board.getPlayerByColor(this.color)) {
+                for (Game b : state) {
+                    score = alphabeta(board.getOpponetn(color).getColor(), b, alpha, beta, depth - 1);
+                    if (score > alpha) {
+                        alpha = score;
+                    }
+                    if (alpha >= beta) {
+                        return alpha;
+                    }
                 }
-                if (alpha >= beta) {
-                    return beta;
+                return alpha;
+            } else {
+                for (Game b : state) {
+                    score =
+                            alphabeta(board.getOpponetn(color).getColor(), b, alpha, beta, depth - 1);
+                    if (score < beta) {
+                        beta = score;
+                    }
+                    if (alpha >= beta) {
+                        return beta;
+                    }
                 }
+
             }
             return beta;
-        }
     }
-
 }
